a61e356c465c1f1e668f9c4ee911dde52b5559b5
JECWorks <36215363+JECWorks@users.noreply.github.com>
2024-11-03 21:25:38 -0500

    Initial Commit

---
+++ iExpense/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iExpense
+//
+//  Created by Jason Cox on 11/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Jason Cox on 11/3/24.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
