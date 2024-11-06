2d0c5bc6e51873ca3228b878c70ba92fb13adbf4
JECWorks <36215363+JECWorks@users.noreply.github.com>
2024-11-05 21:01:52 -0500

    Showing and hiding views

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -5,31 +5,33 @@
 //  Created by Jason Cox on 11/3/24.
 //
 
-import Observation
+
 import SwiftUI
 
-// if you use @State with a struct, it will update the whole view whenever it changes
-// however, if you use @State with a class
-// you mucst mark it as @Observable for it to behave that way
-@Observable
-// this tells SwiftUI to watch each individual property inside
-// the class for changes and reloads any view that relies on
-// a property when it changes
-class User {
-    var firstName = "Bilbo"
-    var lastName = "Baggins"
+struct SecondView: View {
+    @Environment(\.dismiss) var dismiss
+    let name: String
+    var body: some View {
+        Button("Dismiss"){
+            dismiss()
+        }
+    }
 }
 
 struct ContentView: View {
-    @State var user = User()
+@State private var showingSheet: Bool = false
+    
     var body: some View {
         VStack {
-            Text("Your name is \(user.firstName) \(user.lastName)")
-            
-            TextField("First Name", text: $user.firstName)
-            TextField("Last Name", text: $user.lastName)
+            Button("Show Sheet") {
+                showingSheet.toggle()
+            }
+            .sheet(isPresented: $showingSheet) {
+                SecondView(name: "@WinterEC")
+            }
+
         }
-        .padding()
+
     }
 }
 
